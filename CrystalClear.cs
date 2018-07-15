9812718e03a6b85baedb951d1a71d14e4ceb79b0
gnivler <gnivil@gmail.com>
2018-07-15 15:16:56 -0600

    initial commit - v1.0

---
+++ CrystalClear.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine.PostProcessing;
+
+namespace CrystalClear
+{
+    public class CrystalClear
+    {
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("ca.gnivler.ModName");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(DitheringComponent))]
+        [HarmonyPatch("active", PropertyMethod.Getter)]
+        public static class PathDitheringComp
+        {
+            public static bool Prefix()
+            {
+                FileLog.Log($"Dither\n");
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(GrainComponent))]
+        [HarmonyPatch("active", PropertyMethod.Getter)]
+        public static class PatchGrainComp
+        {
+            public static bool Prefix()
+            {
+                FileLog.Log("Grain\n");
+                return false;
+            }
+        }
+    }
+}
